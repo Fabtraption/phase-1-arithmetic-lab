169db42dbe5ac0c312fc8f499d3e2f99f1c6c8dd
Chris Bonet <christopher.bonet@gmail.com>
2022-11-20 15:41:04 -0500

    commit tree d0ec3d41cb4e7ee52af221f116bf77ee2b703f56

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+multiply;
+
+const random = Math.floor(Math.random() * 10);
+
+random;
+
+let num3 = 12;
+let num4 = 3;
+
+const mod = (num3/num4);
+
+mod;
+
+const max = Math.max(5, 10, 15, 20);
+
+max;
